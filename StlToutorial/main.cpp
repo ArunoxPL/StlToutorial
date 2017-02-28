f7c7b28055d8b878ff01df08219791ea5043ec05
ArunoxPL <greg.malek@gmail.com>
2017-02-28 17:21:38 +0100

    Template 1

--- StlToutorial/main.cpp
+++ StlToutorial/main.cpp
@@ -7,6 +7,18 @@ T square(T x)
 	return x*x;
 }
 
+template < typename T >
+class MyVector
+{
+	T arr[1000];
+	int size;
+public:
+	MyVector() :size(0) {}
+	void push(T x) { arr[size] = x; size++; }
+	void print() const { for (int i = 0; i < size; i++) { cout << arr[i] << endl; } }
+};
+
+
 
 int main()
 {
