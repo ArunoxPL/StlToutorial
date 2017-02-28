5b9891bfd4b23fc5d0b5c2bf15b04f5138240410
ArunoxPL <greg.malek@gmail.com>
2017-02-28 16:03:04 +0100

    Template 1

--- StlToutorial/main.cpp
+++ StlToutorial/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+template <typename T>
+
+T square(T x)
+{
+	return x*x;
+}
+
+
 int main()
 {
-	std::cout << "Czy ja dzialam?" << std::endl;
-	printf(" A czy ja dzialam? \n");
+
+	std::cout << square<int>(5) << std::endl;
+	std::cout << square<float>(5.05) << std::endl;
 
 	system("PAUSE");
 	return 0;
